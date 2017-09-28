3fb2f0c119a7fd2e99860933e1380a3571c08b8c
Zhuoqin(Doris) Yu <zhuoqiny@gmail.com>
2017-09-27 22:10:26 -0400

    Add files via upload

--- step2_scraping.py
+++ step2_scraping.py
@@ -23,41 +23,19 @@ for chunk in allchunk:
         if chunk.h3.find("span",{"class":"yloca-tip"})==None:#not Ad
             name=chunk.h3.a.span.get_text().strip()
             one_info["name"]=name
-            one_info["reviews"]=[]
+            one_info["reviews"]=''
             res_url='https://www.yelp.com/' + chunk.h3.find('a').get('href')
             res_r=requests.get(res_url)
             ressoup=BeautifulSoup(res_r.content,'html.parser')
             reviews=ressoup.find_all("p",{"itemprop":"description"})
             if reviews!=None:
                 for rev in reviews:
-                    one_info["reviews"].append(rev.text)
+                    newtext=rev.text
+                    newtext=newtext.replace('\n','')
+                    newtext=newtext.replace('\t','')
+                    one_info["reviews"]=one_info["reviews"]+'|'+newtext
             info.append(one_info)      
-#i=0
-#while(i<4):
-#    try:
-#        i+=1
-#        ttag=soup.find_all('a',{'class':'u-decoration-none next pagination-links_anchor'})
-#        next_url = 'https://www.yelp.com/'+ttag[0].get('href')
-#        q=request.get(next_url)
-#        soup=BeautifulSoup(page.content, 'html.parser')
-#        allchunk= soup.find_all("div",{"class":"media-story"})
-#        for chunk in allchunk:
-#            one_info={}
-#            if chunk.find("h3",{"class":"search-result-title"})!=None:
-#                if chunk.h3.find("span",{"class":"yloca-tip"})==None:#not Ad
-#                    name=chunk.h3.a.span.get_text().strip()
-#                    one_info["name"]=name
-#                    one_info["reviews"]=[]
-#                    res_url='https://www.yelp.com/' + chunk.h3.find('a').get('href')
-#                    res_r=requests.get(res_url)
-#                    ressoup=BeautifulSoup(res_r.content,'html.parser')
-#                    reviews=ressoup.find_all("p",{"itemprop":"description"})
-#                    if reviews!=None:
-#                        for rev in reviews:
-#                            one_info['reviews'].append(rev.text)
-#                    info.append(one_info)
-#    except:
-#        break
+
 data=pd.DataFrame(info)
 #data.to_csv('yelp_downtowndtw_reviews.csv', sep='\t', encoding='utf-8',index=False)
 data.to_csv('yelp_downtowndtw_reviews.csv', sep='\t', encoding='utf-8',mode='a', header=False,index=False)
